f2cace3e5fcc48884fd5df37f0a20c9b254a7001
Mohit Kumar <mohitrath258@gmail.com>
2026-08-24 00:31:48 +0530

    Remove router dependency from DevFlow entrypoint

--- devflow-ai/client/src/main.jsx
+++ devflow-ai/client/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import WorkspaceApp from './WorkspaceApp.jsx'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <WorkspaceApp />
-    </BrowserRouter>
+    <WorkspaceApp />
   </React.StrictMode>
 )
